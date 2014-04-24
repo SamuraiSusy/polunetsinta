8b13687239413b2ab59c99b140362ac20de3b8f0
Susanna Nurmi <susyy-@hotmail.com>
2014-04-24 13:53:32 +0300

    Create AStar.cpp
    added

---
+++ AStar.cpp
@@ -0,0 +1,19 @@
+#include "AStar.h"
+
+AStar::AStar()
+{
+}
+
+void AStar::addNodeToOpenList(Node *node)
+{
+	//node = Node
+	//openList.push_back(node);
+}
+
+void AStar::addNodeToClosedList(Node *node)
+{
+}
+
+AStar::~AStar()
+{
+}
